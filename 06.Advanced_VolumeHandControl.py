ec7bf8556a7089957371a8b0f652f8f060f373ca
sw-song <sw930601@gmail.com>
2021-05-16 19:24:26 +0900

    06th landmark point edit

--- 06.Advanced_VolumeHandControl.py
+++ 06.Advanced_VolumeHandControl.py
@@ -12,9 +12,8 @@ area = 0
 cap = cv2.VideoCapture(0)
 
 #### Object Detection Module
-detector = A_HTM.handDetector()
-s = NSA.alloc().initWithSource_("tell app \"Finder\" to activate")
-s.executeAndReturnError_(None)
+detector = A_HTM.handDetector(maxHands=1)
+
 
 while True:
 
@@ -22,39 +21,40 @@ while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img, draw=True)
+    #cv2.circle(img, lmList[9], 20, (255,255,255))
+    
+    # Display Volume
     cv2.rectangle(img, (50,100), (85,300), (0,0,0), 3)
     script = NSA.alloc().initWithSource_('output volume of (get volume settings)')
     result, error_info = script.executeAndReturnError_(None)
     volume = int(result.stringValue())
-    print(volume)
     cv2.rectangle(img, (52, (302-(2*volume))), (83,298), (0,0,255), cv2.FILLED)
     cv2.putText(img, 'Volume: {} %'.format(volume), (40, 400), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
     
+    # Hand in screen,
     if len(lmList) != 0:
 
-        # Filter based on size
-        area = (bbox[2]-bbox[0])*(bbox[3]-bbox[1])//100
-        # Find Distance Between index and Thumb
+        # Find Distance Between index and Thumb (for control volume with hand gesture)
+        length, img, lineInfo = detector.findDistance(4,8,img)
+        # print(length)
 
         # Convert Volume 
-        # Reduce Resolution to make it smoother
-        # Check Fingers up
-        # If pinky is down set volume
-        # Drawings
-
-
-        x1, y1 = lmList[4][1], lmList[4][2]
-        x2, y2 = lmList[8][1], lmList[8][2]
-        cx12, cy12 = (x1 + x2) // 2, (y1 + y2) // 2
-        cv2.circle(img, (x1,y1), 15, (255,0,255), cv2.FILLED)
-        cv2.circle(img, (x2,y2), 15, (255,0,255), cv2.FILLED)
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-        cv2.circle(img, (cx12,cy12), 10, (255,0,255), cv2.FILLED)
-        length = math.hypot(x2-x1, y2-y1)
         range30to230 = lambda x : 30 if x <= 30 else 230 if x >= 230 else x
-        length100 = (range30to230(length)-30)/2
+        length100 = int((range30to230(length)-30)/2)
+        
+    
+        # Check Fingers up
+        fingers = detector.fingersUp()
+        #if not fingers[2]:
         script = NSA.alloc().initWithSource_('set volume output volume {}'.format(length100))
         script.executeAndReturnError_(None)
+
+        # If pinky is down set volume
+        # Drawings
+        #cv2.rectangle(img, (52, (302-(2*length100))), (83,298), (0,0,255), cv2.FILLED)
+        #cv2.putText(img, 'Volume: {} %'.format(length100), (40, 400), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+    
+
         #volume = int(result.stringValue())
         #cv2.rectangle(img, (50, (100+(2*volume))), (85,300), (0,0,255), cv2.FILLED)
         #cv2.putText(img, 'Volume: {} %'.format(volume), (40, 400), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
--- Advanced_HandTrackingModule.py
+++ Advanced_HandTrackingModule.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
-
+import math 
 class handDetector():
     def __init__(self, mode=False, maxHands=2, detectionCon=0.8, trackCon=0.7):
         self.mode = mode
@@ -12,7 +12,7 @@ class handDetector():
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
-
+        self.tipIds = [4, 8, 12, 16, 20]
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -41,8 +41,9 @@ class handDetector():
                     yList.append(cy)
                     # print(id, cx, cy)
                     self.lmList.append([id, cx, cy])
-                    if draw:
-                        cv2.circle(img, (cx,cy), 5, (255,0,255), cv2.FILLED)
+                    if lmList[9]:
+                        if draw:
+                            cv2.circle(img, (cx,cy), 105, (255,0,255))
             xmin, xmax = min(xList), max(xList)
             ymin, ymax = min(yList), max(yList)
             bbox = xmin, ymin, xmax, ymax
@@ -50,6 +51,36 @@ class handDetector():
             if draw:
                 cv2.rectangle(img, (bbox[0]-20, bbox[1]-20), (bbox[2]+20, bbox[3]+20), (0,0,255), 2)
         return self.lmList, bbox
+
+    def fingersUp(self):
+        fingers = []
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0]-1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # 4 Fingers
+        for id in range(1,5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id]-2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+
+    def findDistance(self, p1, p2, img, draw=True):
+        x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
+        x2, y2 = self.lmList[p2][1], self.lmList[p2][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        
+        if draw:
+            cv2.circle(img, (x1,y1), 15, (255,0,255), cv2.FILLED)
+            cv2.circle(img, (x2,y2), 15, (255,0,255), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (cx,cy), 10, (255,0,255), cv2.FILLED)
+        
+        length = math.hypot(x2-x1, y2-y1)
+        return length, img, [x1, y1, x2, y2, cx, cy]
 
 def main():
     cap = cv2.VideoCapture(0)
